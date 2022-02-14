2a373df6f069b36943ccc894105da43de691ee31
Alexander <sania575@yandex.ru>
2022-02-14 13:38:26 +0300

    lesson

---
+++ 4less.js
@@ -0,0 +1,36 @@
+
+// summ 
+// str + boolean
+console.log('str ' + true);
+// str + numb
+console.log('str ' + 8);
+// numb + boolean
+console.log(999 + true);
+
+// multi
+// str * boolean
+console.log('str ' * true);
+// str * numb
+console.log('str ' * 99);
+// numb * boolean
+console.log(999 * false);
+
+//division
+// str / boolean
+console.log('str ' / true);
+// str / numb
+console.log('str ' / 99);
+// numb / boolean
+console.log(999 / false);
+
+//conversion
+
+let a = '12';
+console.log(typeof Number(a)); 
+
+let b = 1;
+console.log (typeof Boolean(b));
+
+let c = true;
+console.log (typeof String(c));
+
